8e8a2160d584c87f8bd1cb821ddfc26bc1407958
GAURAV SHARMA <87468773+GAURAVSHARMA21804@users.noreply.github.com>
2023-10-13 17:52:00 +0000

    datatypes

---
+++ 01_basics/02_datatypes/datatypes.js
@@ -0,0 +1,32 @@
+/*Data types in JS
+ in the js 7 are premitive data types
+ 1. number => 2 to power 53
+ 2. string => ""
+ 3. boolean => true/false
+ 4. undefined
+ 5. null =>standalone value
+ 6. symbol => unique
+ 7. bigint
+
+ and 3 are the non premitive datatypes
+ 1. obejct
+ 2. functions
+ 3. arrays
+*/
+
+
+"use strict"//treat all JS code as newer version    
+// alert (3+3) we are using node not browser
+
+console.log(3 +
+    3 )//code readbility should be high {ni krna ese}
+
+
+  let age=21
+  let myName = "Gaurav"
+  let state
+  let isLogged = true  
+
+console.table([age,myName,state,isLogged])
+console.log(typeof null)// it gives output  object
+console.log(typeof undefined)//it gives output undefined

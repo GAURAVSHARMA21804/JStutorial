628c4bf307975ab9bcc98d1a78de31481d63fcd6
GAURAV SHARMA <87468773+GAURAVSHARMA21804@users.noreply.github.com>
2023-10-16 21:20:54 +0000

    hi

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,8 @@
+ //array is non premitive datatype collection of multiple items
+ //in js arrays are resizable
+ // 0 based indexing
+ // copy operations create shallow copy and deep copy
+ // shallow copy:- A shallow copy of an object is a copy whose properties share the same refrences
+ // Deep copy:- A Deep copy of an object is a copy whose properties do not share the same refrences
+
+const myArr = [0,1,2,3,4]

d1b3213fc316b2ae6ac20bff16d0b2bcccf7ef99
GAURAV SHARMA <87468773+GAURAVSHARMA21804@users.noreply.github.com>
2023-10-16 14:43:29 +0000

    memory

---
+++ 01_basics/memoryallocation.js
@@ -0,0 +1,24 @@
+// stack (premitive) heap(non-premitive)
+/* stack{
+
+let myname="gaurav"
+let yourName = myname
+yourName = "komal"
+console.log(myname)  // gaurav
+console.log(yourName) // komal
+
+}
+*/
+
+/*heap{
+
+let myobj = {
+    email:"gauravgamail.com"
+}
+
+let myobj2 = myobj
+myobj2.email = "komalgmail.com"
+console.log(myobj2.email)  //komalgmail.com
+console.log(myobj.email)   //komalgmail.com
+
+}*/
